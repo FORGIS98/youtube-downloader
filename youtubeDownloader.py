06f14a7f10fa8533527b431032d6efd85197fee3
FORGIS98 <cifradoforgis@gmail.com>
2019-11-17 00:09:55 +0100

    Getting ready some things.
    So, I just added some extra flags, and global variables. Also the output
    looks prettier.

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os                   # for environment variables
-from shutil import rmtree   # To remove pics directory with the video thumbnail.
 import argparse             # for flags
 import subprocess, shlex    # to call UNIX commands
 
@@ -12,7 +11,9 @@ DEVELOPER_KEY = os.environ['DEV_KEY']
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = 'v3'
 MY_YOUTUBE_SEARCH="https://www.youtube.com/watch?v="
-USER_FOLDER = "\'/home/jorge/Música/%(title)s.%(ext)s\'"
+USER_FOLDER = ""
+USER_FORMAT = ""
+USER_MAX_RESULTS = ""
 
 
 class downloader():
@@ -48,34 +49,31 @@ class downloader():
         yt_response = youtube.search().list(
             q=args.SEARCH, # The parameter q specifies the query term to be searched.
             part='id, snippet', # The part parameter specifies a comma separated list of one or more search resource properties that the API response will include.
-            maxResults=args.max_results, # With the '_' instead of '-'.
-            order="relevance", # Videos will be sorted by visits.
+            maxResults=USER_MAX_RESULTS, # With the '_' instead of '-'.
+            order="relevance", # Videos will be sorted by views.
             regionCode=self.myRegion() # The API will display the search results for the specified country.
         ).execute()
 
         videos = []
-        thumbNails = []
         videoId = []
+        videosChannel = []
 
         for yt_result in yt_response.get('items', []):
             if yt_result['id']['kind'] == "youtube#video":
                 videos.append('%s ' % (yt_result['snippet']['title']))
                 videoId.append('%s ' % (yt_result['id']['videoId']))
-                thumbNails.append('%s ' % (yt_result['snippet']['thumbnails']['default']['url']))
-        self.video_selection(videos, videoId, thumbNails)
+                videosChannel.append('%s ' % (yt_result['snippet']['channelTitle']))
+        self.video_selection(videos, videoId, videosChannel)
 
-    def video_selection(self, videos, videoId, thumbNails):
-        print("This make take a couple of seconds. The script is downloading the thumbnail picture for the user interface.")
+    def video_selection(self, videos, videoId, videosChannel):
         for i in range (len(videos)):
-            print("Video Nº", i+1, ":", videos[i])
-            subprocess.call(shlex.split("wget -q %s" % (thumbNails[i]) + " -O image_%d.jpg" % (i)))
-            subprocess.call(shlex.split("mkdir -p downloaderPics"))
-            subprocess.call(shlex.split("mv ./image_%d.jpg ./downloaderPics" % (i)))
+            print("----------------------------------------------------------------------")
+            print("Video Nº " + "%d" % (i+1) + ": " + videos[i] + "   Uploaded by: " + videosChannel[i])
+        print("----------------------------------------------------------------------")
         print("\n")
 
         x = int(input("Choose the video you like to download: ")) - 1
-        rmtree("downloaderPics") # This will delete the files we just created.
-        com_line = "youtube-dl -x --audio-format mp3 -o "+ USER_FOLDER + " " + MY_YOUTUBE_SEARCH + videoId[x]
+        com_line = "youtube-dl -x --audio-format " + USER_FORMAT + " -o "+ USER_FOLDER + " " + MY_YOUTUBE_SEARCH + videoId[x]
         #subprocess.call(shlex.split(com_line))
         print("CONTROL PRINT")
 
@@ -84,6 +82,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--SEARCH', help="Search from term.", default="youtube")
     parser.add_argument('--max-results', help="Max results output", default=5)
+    parser.add_argument('--folder', help="Folder where files download", default="~/")
+    parser.add_argument('--format', help="mp3 or mp4", default="mp3")
+
     args = parser.parse_args()
+
+    if USER_FOLDER == "":
+        USER_FOLDER = "\'" + args.folder + "%(title)s.%(ext)s\'"
+    if USER_FORMAT == "":
+        USER_FORMAT = args.format
+    if USER_MAX_RESULTS == "":
+        USER_MAX_RESULTS = args.max_results
+
     myDownload = downloader()
     myDownload.yt_search(args)
