3f4d64f547a9429b53f00c786aa3f15860995796
Jorge Sol <jsol@conwet.com>
2021-07-16 13:02:41 +0200

    reviving old projects

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,117 +1,100 @@
 #!/usr/bin/env python3
 
-import os                   # for environment variables
-import argparse             # for flags
-import subprocess, shlex    # to call UNIX commands
+import os
+import argparse
+import subprocess, shlex
 
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 DEVELOPER_KEY = os.environ['DEV_KEY']
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = 'v3'
-MY_YOUTUBE_SEARCH="https://www.youtube.com/watch?v="
-USER_FOLDER_YT = "\'~/Música/%(title)s.%(ext)s\'"
-USER_FOLDER = "~/Música"
-USER_FORMAT = "mp3"
-USER_MAX_RESULTS = "5"
+
+# This is for youtube-dl
+YOUTUBE_DL_URL="https://www.youtube.com/watch?v="
+
+### BEGIN - AUX FUNCTIONS ###
+def getUserRegion():
+    someRegions = {
+        "Madrid" : "ES",
+        "Paris" : "FR",
+        "London" : "GB",
+        "Berlin" : "DE",
+        "Rome" : "IT",
+        "America" : "US"
+    }
+    command = "timedatectl | grep Time"
+    zone = subprocess.getoutput('timedatectl show --va -p Timezone | cut -d\'/\' -f2')
+    try:
+        return someRegions[zone]
+    except KeyError:
+        return "ES"
 
 
-class downloader():
-    # myRegion() will return the ISO 3166 alpha-2 code of yout region.
-    # If your region is not in the list, read README.md for details.
-    def myRegion(self): # returns a STRING
+def changeName():
+    ans = str(input("Do you want to rename the title? [Y/n]: "))
+    if(ans in ["", "Y", "y"]):
+        return str(input("New title: "))
+    return None
+### END - AUX FUNCTIONS ###
 
-        someRegions = {
-            "Madrid" : "ES",
-            "Paris" : "FR",
-            "London" : "GB",
-            "Berlin" : "DE",
-            "Rome" : "IT",
-            "America" : "US"
-        }
 
-        stupidList = ["Madrid", "Paris", "London", "Berlin", "Rome", "America"]
+def yt_search(args):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
-        command = "cat /etc/timezone"
-        # WTF is this, so, .run runs the command, .stdout takes the output, .decode decodes the byte output to string.
-        zone = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode('utf-8')
+    yt_response = youtube.search().list(
+        q=args.SEARCH,
+        part='id, snippet',
+        maxResults=args.max_results,
+        order="relevance",
+        regionCode=getUserRegion()
+    ).execute()
 
-        # TODO: Improve this little function
-        for z in stupidList:
-            if(z in zone):
-                return someRegions[z]
-        return "ES" # Default: Spain
+    videos = []
+    videoId = []
+    videosChannel = []
 
-    # yt_search(args) returns the youtube search specified in --SEARCH flag
-    def yt_search(self, args):
-        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+    for yt_result in yt_response.get('items', []):
+        if yt_result['id']['kind'] == "youtube#video":
+            videos.append('%s ' % (yt_result['snippet']['title']))
+            videoId.append('%s ' % (yt_result['id']['videoId']))
+            videosChannel.append('%s ' % (yt_result['snippet']['channelTitle']))
+    video_selection(videos, videoId, videosChannel, args)
 
-        yt_response = youtube.search().list(
-            q=args.SEARCH, # The parameter q specifies the query term to be searched.
-            part='id, snippet', # The part parameter specifies a comma separated list of one or more search resource properties that the API response will include.
-            maxResults=USER_MAX_RESULTS, # With the '_' instead of '-'.
-            order="relevance", # Videos will be sorted by views.
-            regionCode=self.myRegion() # The API will display the search results for the specified country.
-        ).execute()
 
-        videos = []
-        videoId = []
-        videosChannel = []
+def video_selection(videos, videoId, videosChannel, args):
+    print("")
+    for i in range (len(videos)):
+        print("Video Nº " + "%d" % (i+1) + ": " + videosChannel[i] + " ==> " + videos[i])
+    print("")
+    print("\n")
 
-        for yt_result in yt_response.get('items', []):
-            if yt_result['id']['kind'] == "youtube#video":
-                videos.append('%s ' % (yt_result['snippet']['title']))
-                videoId.append('%s ' % (yt_result['id']['videoId']))
-                videosChannel.append('%s ' % (yt_result['snippet']['channelTitle']))
-        self.video_selection(videos, videoId, videosChannel)
+    # x = int(input("Choose the video you like to download: ")) - 1
+    # com_line = "youtube-dl -x --audio-format " + args.format + " -o "+ args.folder + " " + YOUTUBE_DL_URL + videoId[x]
+    # 
+    # newName = changeName()
+    # if(newName != None):
+    #     subprocess.call(shlex.split(com_line))
+    #     oldOne = os.path.join(args.folder, videos[x])
+    #     newOne = os.path.join(args.folder, newName)
+    #     os.rename(oldOne, newOne)
+    # else:
+    #     subprocess.call(shlex.split(com_line))
 
-    def changeName(self):
-        yesNo = str(input("Do you want to rename the title? [Y/n]: "))
-        y = ""
-        if(yesNo == "" or yesNo == "Y" or yesNo == "y"):
-            y = str(input("New title: "))
-        return y
-
-    def video_selection(self, videos, videoId, videosChannel):
-        for i in range (len(videos)):
-            print("")
-            print("Video Nº " + "%d" % (i+1) + ": " + videos[i] + "   Uploaded by: " + videosChannel[i])
-        print("")
-        print("\n")
-
-        x = int(input("Choose the video you like to download: ")) - 1
-        com_line = "youtube-dl -x --audio-format " + USER_FORMAT + " -o "+ USER_FOLDER_YT + " " + MY_YOUTUBE_SEARCH + videoId[x]
-        y = self.changeName()
-        if(y != ""):
-            subprocess.call(shlex.split(com_line))
-            print("MY SONG BRO :: " + videos[x])
-            oldOne = os.path.join(USER_FOLDER, videos[x])
-            newOne = os.path.join(USER_FOLDER, y)
-            os.rename(oldOne, newOne)
-            # path = "mv " + USER_FOLDER + "/" + videos[x] + " " + USER_FOLDER + "/" + y + "." + USER_FORMAT
-            # print(" Path ::", path)
-            # com_line = path
-            # subprocess.call(shlex.split(com_line))
-        else:
-            subprocess.call(shlex.split(com_line))
-        print("CONTROL PRINT")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--SEARCH', help="Search from term.", default="youtube")
+    parser.add_argument('--SEARCH', help="Search from term", default="Music")
     parser.add_argument('--max-results', help="Max results output", default=5)
-    parser.add_argument('--folder', help="Folder where files download", default="~/")
+    parser.add_argument('--folder', help="Full path to downloads folder", default="~/")
     parser.add_argument('--format', help="mp3 or mp4", default="mp3")
 
     args = parser.parse_args()
 
-    if USER_FOLDER == "":
-        USER_FOLDER_YT = "\'" + args.folder + "/%(title)s.%(ext)s\'"
-        USER_FOLDER = args.folder
-    if USER_FORMAT == "":
-        USER_FORMAT = args.format
-    if USER_MAX_RESULTS == "":
-        USER_MAX_RESULTS = args.max_results
-
-    myDownload = downloader()
-    myDownload.yt_search(args)
+    try:
+        yt_search(args)
+    except HttpError as e:
+        print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+    except Exception as e:
+        print(e)
